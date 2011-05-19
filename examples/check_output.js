010e1ed2fb64382e7c8b95552c4e0332f5a4adf5
guilin 桂林 <guileen@gmail.com>
2011-05-19 12:26:34 +0800

    add --ast -p for debug example

--- examples/check_output.js
+++ examples/check_output.js
@@ -2,7 +2,9 @@
 // -e to execute code
 // no arguments to check output
 var fs = require('fs'),
-    pro = require('../lib/process'),
+    jsp = require('uglify-js').parser,
+    pro = require('uglify-js').uglify,
+    storm = require('../lib/process'),
     vm = require('vm'),
     assert = require('assert');
 
@@ -13,33 +15,37 @@ var args = process.argv.slice(2), arg, options = {
 };
 
 while (arg = args.shift()) {
-  if(arg==='-o'){
+  if (arg === '-o') {
     options.overwrite = true;
     options.check = false;
-  } else if(arg === '-e') {
+  } else if (arg === '--ast') {
+    options.ast = true;
+  } else if (arg === '-e') {
     options.execute = true;
-  } else if(arg === '-nt') {
+  } else if (arg === '-nt') {
     options.check = false;
+  } else if (arg === '-p') {
+    options.print = true;
   } else {
     options.filenames.push(arg);
   }
 }
 
-if(options.filenames.length === 0){
-  options.filenames = ['groups', 'steps', 'serial', 'parallel', 'expression']
+if (options.filenames.length === 0) {
+  options.filenames = ['groups', 'steps', 'serial', 'parallel', 'expression'];
 }
 
-function make_log(filename){
-  var filename = filename + new Array(12-filename.length).join(' ');
-  return function(s){
+function make_log(filename) {
+  var filename = filename + new Array(12 - filename.length).join(' ');
+  return function(s) {
     console.log(filename + ':' + s);
   }
 }
 
-function make_foo(filename){
-  var filename = filename + new Array(12-filename.length).join(' ');
+function make_foo(filename) {
+  var filename = filename + new Array(12 - filename.length).join(' ');
   var log = console.log;
-  return function(){
+  return function() {
     var args = Array.prototype.slice.call(arguments, 0),
         cb = args.pop(),
         arg_name = args.toString(),
@@ -47,7 +53,7 @@ function make_foo(filename){
         ret = Math.floor(Math.random() * 100);
 
     console.log(filename + ':foo(' + arg_name + ')');
-    setTimeout(function(){
+    setTimeout(function() {
         console.log(filename + ':callback ' + arg_name + '#' + ret);
         cb(null, arg_name + '#' + ret);
       }, ret);
@@ -58,9 +64,39 @@ function testFile(filename) {
   filename = filename.replace(/\.js$/, '');
   var code = fs.readFileSync(__dirname + '/' + filename + '.js', 'utf-8');
   var expect = fs.readFileSync(__dirname + '/' + filename + '.out.js', 'utf-8');
-  var target = pro.compile(code);
+  var target;
+  var src_ast = jsp.parse(code);
 
-  if(options.execute){
+  if (options.ast) {
+    console.log(filename + ': source ast');
+    console.log(storm.astToStr(src_ast, ''));
+  }
+
+  if (options.print) {
+    console.log(filename + ': source code');
+    console.log(code);
+  }
+
+  var target_ast = storm.stormfy(src_ast, 'cb');
+
+  if (options.ast) {
+    console.log(filename + ': target ast');
+    console.log(storm.astToStr(target_ast, ''));
+  }
+
+  var target = pro.gen_code(target_ast, {beautify: true});
+
+  if (options.print) {
+    console.log(filename + ': target code');
+    console.log(target);
+  }
+
+  if (options.overwrite) {
+    fs.writeFileSync(__dirname + '/' + filename + '.out.js', target, 'utf-8');
+    expect = target;
+  }
+
+  if (options.execute) {
     var script = vm.createScript(expect);
     var sandbox = {
       foo: make_foo(filename),
@@ -72,11 +108,8 @@ function testFile(filename) {
     script.runInNewContext(sandbox);
   }
 
-  if(options.overwrite){
-    fs.writeFileSync(__dirname + '/' + filename + '.out.js', target, 'utf-8');
-  }
-  if(options.check){
-    assert.equal(target.trim(), expect.trim(), 'error test ' + filename + '.js');
+  if (options.check) {
+    assert.equal(target.trim(), expect.trim(), 'the output is not equals expected. filename: ' + filename + '.js');
   }
 
 }
