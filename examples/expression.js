58e4e2553a7347a368ddc7ce388ad16ac120ed19
guilin 桂林 <guileen@gmail.com>
2011-05-19 16:22:37 +0800

    more test case for expression

--- examples/expression.js
+++ examples/expression.js
@@ -1,5 +1,10 @@
-var a = foo(foo(_) + foo(_), _);
+// expression
+var sum = foo(foo(_) + foo(_), _);
 // empty async call
 foo(_);
 // empty var;
-var x;
+var m;
+// multi var;
+var x = 0, y = 1, z;
+// multi var with callback;
+var a = foo(_), b = foo(_), c = foo(_), d;
--- examples/expression.out.js
+++ examples/expression.out.js
@@ -1,30 +1,67 @@
-var _r1, _r2, a;
+var sum, a, b, c, d;
 
-var count = 2;
-
-function update() {
-    if (--count === 0) {
+var _done = 0, triggers = [ function(_index) {
+    if (3 === (_done & 3)) {
+        triggers.splice(_index, 1);
         foo(_r1 + _r2, function(_err, _r3) {
             if (_err) return cb(_err);
-            a = _r3;
+            sum = _r3;
         });
+    }
+}, function(_index) {
+    if (28 === (_done & 28)) {
+        triggers.splice(_index, 1);
+        a = _r5;
+        b = _r6;
+        c = _r7;
+    }
+} ];
+
+function trigger() {
+    for (var i = triggers.length - 1; i >= 0; i--) {
+        triggers[i](i);
     }
 }
 
 foo(function(_err, __r1) {
     if (_err) return cb(_err);
     _r1 = __r1;
-    update();
+    _done |= 1;
+    process.nextTick(trigger);
 });
 
 foo(function(_err, __r2) {
     if (_err) return cb(_err);
     _r2 = __r2;
-    update();
+    _done |= 2;
+    process.nextTick(trigger);
 });
 
 foo(function(_err, _r4) {
     if (_err) return cb(_err);
 });
 
-var x;
+var m;
+
+var x = 0, y = 1, z;
+
+foo(function(_err, __r5) {
+    if (_err) return cb(_err);
+    _r5 = __r5;
+    _done |= 4;
+    process.nextTick(trigger);
+});
+
+foo(function(_err, __r6) {
+    if (_err) return cb(_err);
+    _r6 = __r6;
+    _done |= 8;
+    process.nextTick(trigger);
+});
+
+foo(function(_err, __r7) {
+    if (_err) return cb(_err);
+    _r7 = __r7;
+    _done |= 16;
+    process.nextTick(trigger);
+});
